9e806c8956afd303d60fc2123dcba0a091faabac
Gerardo Morales <geramoralesrico+code@gmail.com>
2022-03-12 12:17:03 -0600

    reset edit state after submitting

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -43,6 +43,10 @@ export const FeedbackProvider = ({ children }) => {
 
   const updateFeedback = (id, updItem) => {
     setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)))
+    setFeedbackEdit({
+      item: {},
+      edit: false,
+    })
   }
 
   //Set item to be updated
